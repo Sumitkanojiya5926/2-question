d1631f899bd3d42d3d4d009d969735c58b578139
Sumitkanojiya5926 <sumitkanojiya9328@gmail.com>
2025-03-18 20:49:42 +0530

    Add files via upload

---
+++ 2question.py
@@ -0,0 +1,20 @@
+a = int(input())
+b = int(input())
+
+sum = a + b 
+print(f"Sum of two{sum}")
+
+difference = a - b
+print(f"gape of two number is {difference}")
+
+multiplication = a ** b
+print(f"multiplication of two number {multiplication}")
+
+"""a = int(input())
+b = int(input())
+
+print (a + b) 
+
+print( a - b)
+
+print(a * b)"""
